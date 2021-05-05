553ce1774ea0fe678c71edb05484c75f8f9e89b6
luojinrui <luojinrui@kingsoft.com>
2021-05-05 20:50:10 +0800

    init

---
+++ src/components/Index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="indexPage">
+    <el-container style="height: 100%">
+      <el-header class="top" height="60px">Header</el-header>
+      <el-container>
+        <el-aside class="leftAside" width="250px">
+          Aside
+        </el-aside>
+        <el-main>
+          Main
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.indexPage {
+  height: 100vh;
+  background: gray;
+}
+.top {
+  background: red;
+}
+.leftAside {
+  background: green;
+}
+</style>
